93567243f56ec247fee67c429075093811e7ccb6
EliaukRender <chang_showing@163.com>
2023-08-24 00:49:03 +0800

    修改uploadDialog组件

---
+++ src/components/UploadDialog/constant/enum.js
@@ -0,0 +1,16 @@
+ // 文件类型图标
+ export const fileTypeEnum = {
+  '.xls': 'excel',
+  '.xlsx': 'excel',
+  '.doc': 'word',
+  '.docx': 'word',
+  '.pdf': 'pdf',
+  '.zip': 'zip',
+  '.rar': 'zip',
+  '.png': 'image',
+  '.jpeg': 'image',
+  '.jpg': 'image',
+  '.gif': 'image',
+  '.mp4': 'video',
+  '.avi': 'video',
+}
